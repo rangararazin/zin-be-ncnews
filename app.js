02b1abfcd01b8f6b2be6749097ead385874aec16
rangararazin <rangararazin@outlook.com>
2022-11-14 15:09:06 +0000

    implement get all articles in descending order based on created date

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getTopics } = require("./controllers/news-controller");
+const { getTopics, getArticles } = require("./controllers/news-controller");
 
 const { handleCustomError } = require("./errors/errors");
 const app = express();
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles", getArticles);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ msg: "URL not found" });
