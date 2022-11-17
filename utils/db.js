499545d098119c1f84e524c39b15d16b9d2c53d7
rangararazin <rangararazin@outlook.com>
2022-11-17 09:14:01 +0000

    Implemet queries for articles wih error handling

--- utils/db.js
+++ utils/db.js
@@ -29,3 +29,20 @@ exports.checkUserExist = (column, value) => {
     }
   });
 };
+
+exports.checkTopicsExist = (topic) => {
+  return db
+    .query(
+      `
+SELECT * FROM topics
+WHERE slug=$1`,
+      [topic]
+    )
+    .then((result) => {
+      if (!result.rows.length) {
+        return Promise.reject({ status: 404, msg: "Topic not found" });
+      } else {
+        return topic;
+      }
+    });
+};
