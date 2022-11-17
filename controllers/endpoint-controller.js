5b61d7f7dc67d41b58430c0d2afd73db312eec92
rangararazin <rangararazin@outlook.com>
2022-11-17 12:25:40 +0000

    Implemenet get all endpoints of api

--- controllers/endpoint-controller.js
+++ controllers/endpoint-controller.js
@@ -2,7 +2,6 @@ const { readEndpoints } = require("../models/endpoint-model");
 
 exports.getEndpoints = (req, res, next) => {
   readEndpoints().then((endpoint) => {
-    console.log(endpoint);
     res.status(200).send({ endpoints: endpoint });
   });
 };
